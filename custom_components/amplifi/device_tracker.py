c7506a676fba27b5a506fab0dd689c4b5590bcb1
hawksj <58028821+hawksj@users.noreply.github.com>
2024-04-24 15:49:08 +0100

    Update device_tracker.py

--- custom_components/amplifi/device_tracker.py
+++ custom_components/amplifi/device_tracker.py
@@ -8,7 +8,7 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.components.device_tracker.config_entry import ScannerEntity
 from homeassistant.components.device_tracker import SourceType
 from homeassistant.core import callback
-from .const import DOMAIN, COORDINATOR, COORDINATOR_LISTENER, ENTITIES
+from .const import DOMAIN, COORDINATOR, COORDINATOR_LISTENER, ENTITIES, CONF_ENABLE
 from .coordinator import AmplifiDataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
@@ -144,6 +144,11 @@ class AmplifiWifiDeviceTracker(CoordinatorEntity, ScannerEntity):
         if self.coordinator.last_update_success and self._data is not None:
             return {**self._data, "last_seen": datetime.now().isoformat()}
         return {}
+
+    @property
+    def entity_registry_enabled_default(self) -> bool:
+        """Return if the entity should be enabled when first added to the entity registry."""
+        return hass.data[CONF_ENABLE]
 
     def update(self):
         _LOGGER.debug(f"entity={self.unique_id} update() was called")
